5c12f9c72f70902d89a3564b6cb18487ec45f96f
Rodrigo Thenopholo <thenopholo92@icloud.com>
2025-07-04 10:08:50 -0300

    feat: Add API server and user service handler implementation

---
+++ cmd/api/api.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/thenopholo/ecom_backend/internal/service/user"
+)
+
+
+type APIServer struct {
+  addr string
+  db *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+  return &APIServer{
+  	addr: addr,
+  	db:   db,
+  }
+}
+
+func (s *APIServer) Run() error {
+  router := mux.NewRouter()
+  subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+  userHandler := user.NewHandler()
+  userHandler.RegisterRouter(subrouter)
+
+  log.Printf("Listen on %s\n", s.addr)
+
+  return http.ListenAndServe(s.addr, router)
+}
